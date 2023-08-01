6bba1d29cf8e6760a3c58d05c28e670528035630
ztao2 <ztao2@uw.edu>
2023-07-31 21:22:38 -0700

    merge

--- backend/api/views.py
+++ backend/api/views.py
@@ -90,6 +90,7 @@ def getRedirect(request):
         'https://api.human-id.org/v1/server/users/web-login',
         headers=headers,
     )
+
     if response.status_code != 200:
         return Response("Unable to generate url. Please double check your credentials", status=403)
 
@@ -236,24 +237,23 @@ def verification_successful(request):
 
     requestId = resJson['data']['requestId']
     humanUserId = resJson['data']['appUserId']
-    # In case the request id doesnt exist
+
+    # in case the request id doesnt exist
     reqExist = Request.objects.filter(requestId=requestId).exists()
     if not reqExist:
         return Response("The server returned a request id of requestId, which does not match our records.", status=400)
 
-    # Check if the discord userId from the request matches the one from the humanID server
-    # TODO: Check if every server creates different clientID
+    # check if the humanID user already has an associated account for the server
     associatedAccount = Person.objects.filter(humanUserId=humanUserId).exists()
-    associatedAccountUser = Person.objects.filter(humanUserId=humanUserId).first()
     req = Request.objects.get(requestId=requestId)
-    if associatedAccount and not verify(req.userId, associatedAccountUser.userId):
+    if associatedAccount and not verify(req.userId, associatedAccount.userId):
         return Response(
             'The provided credentials are already associated with another user in the server with the server id {}'.format(serverQuery),
             status=409
         )
 
     elif not associatedAccount:
-        # Associate the humanID user with their discord id
+        # associate the humanID user with their discord id
         Person.objects.create(
             humanUserId=humanUserId,
             userId=req.userId,
