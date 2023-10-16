d980550682cd394b4cd098c8893afa431ba37df1
vxcent <tedxu1111@gmail.com>
2023-10-16 11:35:06 -0700

    Fixed metadata fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import Home from './pages/Home.jsx';
-import {Routes, Route, BrowserRouter}  from "react-router-dom";
-import './App.css';
+import React from "react";
+import Home from "./pages/Home.jsx";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
 import VerificationSuccessful from "./pages/VerificationSuccessful";
 import VerificationFailed from "./pages/VerificationFailed";
-
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path={"/"} element={<Home/>}/>
-      <Route path={"/verification_successful"} element={<VerificationSuccessful/>}/>
-      <Route path={"/verification_failed"} element={<VerificationFailed/>}/>
-    </Routes>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route
+          path={"/verification_successful"}
+          element={<VerificationSuccessful />}
+        />
+        <Route path={"/verification_failed"} element={<VerificationFailed />} />
+      </Routes>
     </BrowserRouter>
   );
 }
