5a8f799bdd4dc482c5cbcfc3cc5dda11a40cfecb
Lunasocute <77700070+Lunasocute@users.noreply.github.com>
2024-08-09 13:38:05 -0700

    Update discordbot.py

--- bot/discordbot.py
+++ bot/discordbot.py
@@ -590,7 +590,8 @@ async def on_member_update(before, after):
     user_id = after._user.id
     server_id = after.guild.id
     log_channel = await ensure_text_channel(guild, None, "logs")
-
+    if not log_channel:
+        return
     try:
         if "humanID-Verified" in [role.name for role in before.roles] and "humanID-Verified" not in [role.name for role in after.roles]:
             response = requests.delete(
@@ -599,18 +600,18 @@ async def on_member_update(before, after):
             if response.status_code == 200:
                 success = True
             if success:
-                if not log_channel:
-                    return
                 await log_channel.send(f"humanID-verified role has been removed for user {before.name}")
     except Exception as e:
         await log_channel.send(f"Error occurred while deleting {before.name} from database: {str(e)}")
         
 @bot.event
 async def on_member_remove(member):
+    user_id = member.id
+    server_id = member.guild.id
+    BACKEND_URL = env["DISCORD_BACKEND_URL"]
+    if not log_channel:
+        return
     try:
-        user_id = member.id
-        server_id = member.guild.id
-        BACKEND_URL = env["DISCORD_BACKEND_URL"]
         response = requests.delete(
             BACKEND_URL + '/api/removeUser/?userId={}&serverId={}'.format(user_id,server_id)
         )
@@ -618,12 +619,9 @@ async def on_member_remove(member):
         if response.status_code == 200:
             success = True
         if success:
-            if not log_channel:
-                return
             await log_channel.send(f"{member.name} has left the server or is kicked-off from the server")
     except Exception as e:
         await log_channel.send(f"Error occurred while deleting {member.name} from database: {str(e)}")
-
 
 # await message.channel.send(embed=embed, view=view)
 
